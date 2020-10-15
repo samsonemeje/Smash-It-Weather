fe0ab4ed79124a583627965f417b782ee839e7a2
Samsonemeje <samsonemeje@gmail.com>
2020-10-15 12:16:52 +0100

    add functions to get data/submit input form

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,44 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then((reg) => console.log('service worker registered', reg))
-    .catch((err) => console.log('service worker not registered', err));
-}
+// select elements id //
+const nameValue = document.querySelector('#name');
+const dateValue = document.querySelector('#date');
+const timeValue = document.querySelector('#time');
+const tempValue = document.querySelector('#temp');
+const weatherValue = document.querySelector('#weather');
+
+const inputField = document.querySelector('#input_field');
+const form = document.querySelector('#form');
+
+const weatherContent = document.querySelector('#weather_content');
+
+let WeatherArray = JSON.parse(localStorage.getItem('weatherdata')) || [];
+
+// function to get weather data //
+const GetWeather = (Location) => {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${Location}&appid=f341c5aa8a6091cd451a8525cc820c84`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      WeatherArray.push(data);
+      console.log(data);
+      console.log(WeatherArray);
+      localStorage.setItem('weatherdata', JSON.stringify(WeatherArray));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// function to submit the input form //
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  GetWeather(inputField.value);
+
+  console.log(inputField.value, 'I am submited');
+
+  inputField.value = '';
+};
+
+form.addEventListener('submit', handleSubmit);
+console.log(WeatherArray);
--- public/js/ui.js
+++ public/js/ui.js
@@ -1,69 +1,69 @@
-const api = {
-  key: 'f341c5aa8a6091cd451a8525cc820c84',
-  base: 'https://api.openweathermap.org/data/2.5/',
-};
+// const api = {
+//   key: 'f341c5aa8a6091cd451a8525cc820c84',
+//   base: 'https://api.openweathermap.org/data/2.5/',
+// };
 
-const searchbox = document.querySelector('.search-box');
-searchbox.addEventListener('keypress', setQuery);
+// const searchbox = document.querySelector('.search-box');
+// searchbox.addEventListener('keypress', setQuery);
 
-function setQuery(evt) {
-  if (evt.keyCode == 13) {
-    getResults(searchbox.value);
-  }
-}
+// function setQuery(evt) {
+//   if (evt.keyCode == 13) {
+//     getResults(searchbox.value);
+//   }
+// }
 
-function getResults(query) {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then((weather) => {
-      return weather.json();
-    })
-    .then(displayResults);
-}
+// function getResults(query) {
+//   fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+//     .then((weather) => {
+//       return weather.json();
+//     })
+//     .then(displayResults);
+// }
 
-function displayResults(weather) {
-  let city = document.querySelector('.detail-location');
-  city.innerText = `${weather.name}, ${weather.sys.country}`;
+// function displayResults(weather) {
+//   let city = document.querySelector('.detail-location');
+//   city.innerText = `${weather.name}, ${weather.sys.country}`;
 
-  let now = new Date();
-  let date = document.querySelector('.detail-date');
-  date.innerText = dateBuilder(now);
+//   let now = new Date();
+//   let date = document.querySelector('.detail-date');
+//   date.innerText = dateBuilder(now);
 
-  let temp = document.querySelector('.detail-temp');
-  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+//   let temp = document.querySelector('.detail-temp');
+//   temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
-  let weather_el = document.querySelector('.detail-info');
-  weather_el.innerText = weather.weather[0].main;
-}
+//   let weather_el = document.querySelector('.detail-info');
+//   weather_el.innerText = weather.weather[0].main;
+// }
 
-function dateBuilder(d) {
-  let months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  let days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ];
+// function dateBuilder(d) {
+//   let months = [
+//     'January',
+//     'February',
+//     'March',
+//     'April',
+//     'May',
+//     'June',
+//     'July',
+//     'August',
+//     'September',
+//     'October',
+//     'November',
+//     'December',
+//   ];
+//   let days = [
+//     'Sunday',
+//     'Monday',
+//     'Tuesday',
+//     'Wednesday',
+//     'Thursday',
+//     'Friday',
+//     'Saturday',
+//   ];
 
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
+//   let day = days[d.getDay()];
+//   let date = d.getDate();
+//   let month = months[d.getMonth()];
+//   let year = d.getFullYear();
 
-  return `${day} ${date} ${month} ${year}`;
-}
+//   return `${day} ${date} ${month} ${year}`;
+// }
